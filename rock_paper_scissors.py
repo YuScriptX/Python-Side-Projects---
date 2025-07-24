e14737fbc67da7594cf795c50898d7969cca9a31
YuScriptX <yusuf.tiftik@outlook.com>
2025-07-24 16:39:42 +0200

    added a while function for to choose only rock, paper or scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,8 +6,8 @@ game = ("rock" , "paper" , "scissors")
 player = None
 computer = random.choice(game)
 
-
-player = input("Make choice between rock, paper and scissors: ")
+while player not in game:
+    player = input("Make choice between rock, paper and scissors: ")
 
 print(f"Player: {player}")
 print(f"Computer: {computer}")

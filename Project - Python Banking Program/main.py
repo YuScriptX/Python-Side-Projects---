fe9381fcb5c212f810d341f4776138f2e6354b88
YuScriptX <yusuf.tiftik@outlook.com>
2025-08-07 10:41:25 +0200

    Add main loop with menu options for balance, deposit, withdraw, and exit

--- Project - Python Banking Program/main.py
+++ Project - Python Banking Program/main.py
@@ -12,3 +12,21 @@ def withdraw():
 balance = 0
 is_running = True
 
+while is_running:
+    print("1.Show balance")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+
+    choice = input("Enter a choice between 1 & 4: ")
+
+    if choice == '1':
+        show_balance()
+    elif choice == '2':
+        balance()
+    elif choice == '3':
+        deposit()
+    elif choice == '4':
+        withdraw()
+    else:
+        print("It is invalid")

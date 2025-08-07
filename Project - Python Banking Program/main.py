7d7c72e47f738201c44fd6f7a5ba435143e47894
YuScriptX <yusuf.tiftik@outlook.com>
2025-08-07 11:09:30 +0200

    Add a function main

--- Project - Python Banking Program/main.py
+++ Project - Python Banking Program/main.py
@@ -1,6 +1,6 @@
 # Python Banking Program
 
-def show_balance():
+def show_balance(balance):
     print(f"Show the balance: €{balance:.2f} ")
 
 def deposit():
@@ -12,7 +12,7 @@ def deposit():
     else:
         return amount
 
-def withdraw():
+def withdraw(balance):
     amount = float(input("How much do you wanna withdraw: "))
 
     if amount > balance:
@@ -24,27 +24,30 @@ def withdraw():
     else:
         return amount
 
-balance = 0
-is_running = True
 
-while is_running:
-    print("Banking Program")
-    print("1.Show balance")
-    print("2.Deposit")
-    print("3.Withdraw")
-    print("4.Exit")
+def main():
 
-    choice = input("Enter a choice between 1 & 4: ")
+    balance = 0
+    is_running = True
 
-    if choice == '1':
-        show_balance()
-    elif choice == '2':
-        balance += deposit()
-    elif choice == '3':
-        balance -= withdraw()
-    elif choice == '4':
-        exit()
-    else:
-        print("It is invalid")
 
-print("Thank you!")
+    while is_running:
+        print("Banking Program")
+        print("1.Show balance")
+        print("2.Deposit")
+        print("3.Withdraw")
+        print("4.Exit")
+
+        choice = input("Enter a choice between 1 & 4: ")
+
+        if choice == '1':
+            show_balance(balance)
+        elif choice == '2':
+            balance += deposit()
+        elif choice == '3':
+            balance -= withdraw(balance)
+        elif choice == '4':
+            exit("Thank you!")
+        else:
+            print("It is invalid")
+main()

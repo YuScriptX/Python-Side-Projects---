46f5fec0c632229233a3af33753a2fb0d7110404
YuScriptX <yusuf.tiftik@outlook.com>
2025-08-10 16:45:09 +0200

    Implement main loop for slot machine with balance and bet validation

--- Project - Slot Machine/main.py
+++ Project - Slot Machine/main.py
@@ -10,7 +10,22 @@ def get_payout():
     pass
 
 def main():
-    pass
+    balance = 1000
+
+    print("/****** Welcome to the game! ******/")
+    print("Symbols : 🍉 🍋 🍒 ⭐")
+    print("/*************************/")
+
+    while balance > 0:
+        print(f"Your balance is: € {balance}")
+
+        bet = input("How much you wanna bet: ")
+
+        if not bet.isdigit():
+            print("Enter a valid number")
+            continue
+
+        bet = int(bet)
 
 if __name__ == '__main__':
     main()

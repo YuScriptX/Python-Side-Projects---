9982062d7cca4628346627f0d4c18a8d8a223495
YuScriptX <yusuf.tiftik@outlook.com>
2025-08-20 16:49:59 +0200

    Implement weight conversion logic between Kg and Lbs with validation

--- Project - Weight Converter/main.py
+++ Project - Weight Converter/main.py
@@ -2,3 +2,12 @@
 
 weight = float(input("Enter your weight: "))
 unit = str(input("Kg or Lbs: "))
+
+if unit == "Kg":
+    weight = weight * 2.205
+elif unit == "Lbs":
+    weight = weight / 2.205
+else:
+    print("Invalid value")
+
+print(f"Your weight is: {weight} {unit}")

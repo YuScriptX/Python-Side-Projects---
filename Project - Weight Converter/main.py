066abc96dc418574a3dd4a3c600df28c796e2528
YuScriptX <yusuf.tiftik@outlook.com>
2025-08-20 16:52:32 +0200

    Refactor Weight Converter to print conversion results inside condition blocks

--- Project - Weight Converter/main.py
+++ Project - Weight Converter/main.py
@@ -6,10 +6,10 @@ unit = str(input("Kg or Lbs: "))
 if unit == "Kg":
     weight = weight * 2.205
     unit = "Lbs."
+    print(f"Your weight is: {weight} {unit}")
 elif unit == "Lbs":
     weight = weight / 2.205
     unit = "Kg."
+    print(f"Your weight is: {weight} {unit}")
 else:
     print("Invalid value")
-
-print(f"Your weight is: {weight} {unit}")

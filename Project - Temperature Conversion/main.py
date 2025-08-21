4e0aa282c778b7dfbf6bf5599e648575497b1eaa
YuScriptX <yusuf.tiftik@outlook.com>
2025-08-21 15:36:39 +0200

    Set up structure for Celsius/Fahrenheit temperature conversion

--- Project - Temperature Conversion/main.py
+++ Project - Temperature Conversion/main.py
@@ -2,3 +2,10 @@
 
 unit = input("Is this tmeperature in Celcius or Fahrenheit (C/F): ")
 temp = float(input("ENter the temperature: "))
+
+if unit == "C":
+    pass
+elif unit == "F":
+    pass
+else:
+    pass

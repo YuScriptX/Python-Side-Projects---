0f02ba77df9854596249866086effd8891e20053
YuScriptX <yusuf.tiftik@outlook.com>
2025-08-28 08:54:19 +0200

    Add compound interest calculation and display total balance

--- Project - Compound Interest Calculator/main.py
+++ Project - Compound Interest Calculator/main.py
@@ -19,6 +19,5 @@ while time <=0:
     if time <= 0:
         print("Time can't be less than or equal to zero")
 
-print(principle)
-print(rate)
-print(time)
+total = principle * pow((1 + rate / 100), time)
+print(f"Balance after {time} years/s: €{total:.2f}")

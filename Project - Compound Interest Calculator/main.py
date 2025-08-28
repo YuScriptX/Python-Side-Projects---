d30a5bc1bd0a9e3fd4a81bfdc9a95c4a19f2936a
YuScriptX <yusuf.tiftik@outlook.com>
2025-08-28 08:44:40 +0200

    Add validation to ensure principle amount is greater than zero + testing

--- Project - Compound Interest Calculator/main.py
+++ Project - Compound Interest Calculator/main.py
@@ -3,3 +3,10 @@
 principle = 0
 rate = 0
 time = 0
+
+while principle <=0:
+    principle = float(input("Enter the principle amount:"))
+    if principle <= 0:
+        print("Principle can't be less than or equal to zero")
+
+print(principle)

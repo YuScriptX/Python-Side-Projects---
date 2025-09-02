4ea8d109171089042316c65312e33453143ad426
YuScriptX <yusuf.tiftik@outlook.com>
2025-09-02 16:21:26 +0200

    Add formatted countdown timer with hours, minutes, and seconds display

--- Project - Countdown Timer/main.py
+++ Project - Countdown Timer/main.py
@@ -4,8 +4,11 @@ import time
 
 my_time = int(input("Enter the time in sec: "))
 
-for x in range(0, my_time):
-    print(x)
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int( x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
     time.sleep(1)
 
 print("Hi Guys!")

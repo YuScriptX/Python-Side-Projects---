e9e8cbd4347593e3bf02980af74456f9da93b90b
YuScriptX <yusuf.tiftik@outlook.com>
2025-09-08 16:33:13 +0200

    Add quiz questions, options, and correct answers

--- Project - Quiz Game (Intermediate)/main.py
+++ Project - Quiz Game (Intermediate)/main.py
@@ -1,1 +1,14 @@
 # Quiz Game (Intermediate)
+
+questions = ("What color is the sky on a clear day?",
+             "Which animal says meow?",
+             "What do you use to write on paper?")
+
+options = (("A. White","B. Blue","C. Grey"),
+           ("A. Dog","B. Mouse","C. Cat"),
+           ("A. Pen","B. Scissor","C. Highlighter"))
+
+answers = ("B","C","A")
+guesses = []
+score = 0
+question_num = 0

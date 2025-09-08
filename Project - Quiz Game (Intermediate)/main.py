b49bdc206ebf1ae2020a1af5b81f570e5dfc113f
YuScriptX <yusuf.tiftik@outlook.com>
2025-09-08 16:59:29 +0200

    Add quiz results summary showing correct answers and user guesses

--- Project - Quiz Game (Intermediate)/main.py
+++ Project - Quiz Game (Intermediate)/main.py
@@ -28,3 +28,17 @@ for question in questions:
         print("INCORRECT!")
         print(f"{answers[question_num]} is the correct answer")
     question_num += 1 
+
+print("---------------")
+print("   RESULTS    ")
+print("---------------")
+
+print("answers: ", end="")
+for answer in answers:
+    print(answer, end=" ")
+print()
+
+print("guesses: ", end="")
+for guess in guesses:
+    print(guess, end=" ")
+print()

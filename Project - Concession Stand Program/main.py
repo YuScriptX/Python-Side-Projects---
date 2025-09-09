72afe8cb23855bce4f86078e27191e1aa57ca9df
YuScriptX <yusuf.tiftik@outlook.com>
2025-09-09 17:01:39 +0200

    Define concession stand menu with items, prices, and initialize cart

--- Project - Concession Stand Program/main.py
+++ Project - Concession Stand Program/main.py
@@ -1,2 +1,11 @@
 # Concession Stand Program
 
+menu = {"soda": 3.00,
+        "nachos": 5.00,
+        "chips": 7.00,
+        "hotdog": 9.00,
+        "kebab": 12.00,
+        "pizza": 15.00,}
+
+cart = []
+total = 0

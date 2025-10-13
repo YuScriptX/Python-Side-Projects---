5a97fc516442a98e7b6f3511a344ea10db86b195
YuScriptX <yusuf.tiftik@outlook.com>
2025-10-13 12:41:42 +0200

    add alarm loop with real-time check and wake-up trigger

--- Project - Alarm Clock/main.py
+++ Project - Alarm Clock/main.py
@@ -7,6 +7,18 @@ import pygame
 def set_alarm(alarm_time):
     print(f"Alarm set for {alarm_time}")
     sound_file ="music_mp3.mp3"
+    is_running = True
+
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+
+        if current_time == alarm_time:
+            print("WAKE UP!")
+            
+            is_running = False
+
+        time.sleep(1)
 
 if __name__ == "__main__":
     alarm_time = input("Enter the alarm time (HH:MM:SS): ")

52451c209724c7c097fb1cfa353fc4e1af85d0d4
YuScriptX <yusuf.tiftik@outlook.com>
2025-10-20 15:57:48 +0200

    add basic WeatherApp skeleton with PyQt5 setup

--- Weather app (Intermediate)/main.py
+++ Weather app (Intermediate)/main.py
@@ -1,2 +1,12 @@
 # Weather App
 
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtCore import QTimer, QTime, Qt
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)

ca66dcbe0375a36421fed9dfe4ce80ae676a6c07
YuScriptX <yusuf.tiftik@outlook.com>
2025-10-20 16:00:09 +0200

    add WeatherApp skeleton with requests and PyQt5 widget imports

--- Weather app (Intermediate)/main.py
+++ Weather app (Intermediate)/main.py
@@ -1,8 +1,9 @@
 # Weather App
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
-from PyQt5.QtCore import QTimer, QTime, Qt
+import requests
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout)
+from PyQt5.QtCore import Qt
 
 class WeatherApp(QWidget):
     def __init__(self):
